5c24baa6028afd8ac4ce2d53ac6b50dd609d8cca
Renan Breves <brevesrenan25@gmail.com>
2023-01-20 17:31:29 -0300

    Commit principal

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//ativar links menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+//ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
